b9a684850a7eb8c058ffc2a28ea73b81f569dbd6
paptim01 <paptim01@students.zhaw.ch>
2025-01-22 14:17:30 +0100

    Main Class

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(){
+
+        System.out.println("Welcome to Blackjack!");
+        while(true){
+            continue;
+        }
+    }
+}
